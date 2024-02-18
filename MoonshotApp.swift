f991c40b76c28c02f47ccbca8e9825400ea2b311
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-02-18 15:02:14 +0800

    Initial commit

---
+++ MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Liko Setiawan on 18/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
